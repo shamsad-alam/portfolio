6c5354d0988134e1cb705ba04086326c3149acb6
shamsad12921 <shamsadalam604@gmail.com>
2023-12-19 21:29:13 +0530

    first commit

---
+++ navbar.js
@@ -0,0 +1,12 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+function togle2() {
+    var nave = document.querySelector("#menu");
+    if (nave.style.display == "block") {
+        nave.style.display = "none";
+    } else {
+        nave.style.display = "block";
+    }
+}
